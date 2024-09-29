bf56e334d8ea331f29019c79cb0a45cdf157e0a0
Arun-kh-kannery <arunkh65328@gmail.com>
2024-09-29 22:00:04 +0530

    cscs

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  base_url: "http://hph.05f.mytemp.website/api",
-  image_url: "http://hph.05f.mytemp.website",
+  base_url: "https://hph.05f.mytemp.website/api",
+  image_url: "https://hph.05f.mytemp.website",
 };
 export default config;
